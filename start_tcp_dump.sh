6b0e0a97ad72c426c025b9ff28838ba2ab940769
maxspiff <massimo.iannelli@gmail.com>
2017-03-22 11:33:22 +0100

    tcpdump script first commit

---
+++ start_tcp_dump.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+# script for tcpdump
+# by Massimo 23/12/2016
+# version 1.0
+
+#variables
+PORTPAR=$2
+IFPAR=$1
+FSYS=/var/tmp
+HST="`hostname`"
+TCPDUMP_PID=0
+TCPDUMP_PID_FILE=$FSYS/tcpdump.pid
+
+
+check_intf_par () {
+        if [ $IFPAR ] 
+        then {
+#                echo "Interface par = $IFPAR"
+                INTF=`ifconfig -a |grep $IFPAR |awk '{print $1}'`
+#                echo "interface par after check =$INTF"
+            if [ $INTF ]
+                then {
+                        INTF=$IFPAR
+                        echo "found interface $INTF"
+                } else {
+                        echo "interface $IFPAR does not exist Exiting"
+                        exit
+                }
+                fi
+        } else {
+                echo "Missing interface parameter exiting"
+                exit
+        }
+        fi
+}
+
+check_port () {
+        NPORT=`netstat -an |grep LISTEN |grep -v LISTENING|grep ${PORTPAR} |wc -l`
+
+#       echo "in check port PORTPAR=$NPORT"
+        if [ $NPORT -eq 0 ]
+        then {
+                echo "Port $PRT is not listening exiting"
+                exit
+        }
+        fi
+}
+
+check_port_par () {
+        if [ $PORTPAR ] 
+        then {
+                PRT=$PORTPAR
+                check_port
+        } else {
+                LOG_FILE="$FSYS/tcpdump_INTF_`date +%d%m%y`.pcap"
+                PRT=0
+        }
+        fi
+}
+
+
+check_fs () {
+        RESFS=$(df -h $FSYS |tail -1|awk '{print $5}'|tr -d %)
+        echo "in check_fs par=$RESFS"
+        if [ $RESFS -gt 85 ] 
+        then {
+                echo "File system $FSYS is $RESFS% tcpdump will not start"
+                exit
+        } 
+        fi
+}
+
+
+
+start_tcp () {
+	echo "in start tcp"
+        if [ $PRT -eq 0 ] 
+        then {
+                FNAME="${FSYS}/${HST}_tcpdump_${INTF}_`date +%d%m%y_%H%M`.pcap"
+                echo "Starting tcpdump on interface ${INTF}, File system ${FSYS} is ${RESFS}%"
+                2>/dev/null 1>&2 tcpdump -i ${INTF} -s0 -C 100 -w "$FNAME" &
+                TCPDUMP_PID=$!
+                echo "file created : $FNAME with PID=${TCPDUMP_PID}"
+                echo "$TCPDUMP_PID">$TCPDUMP_PID_FILE
+
+        } else {
+		echo "in else start_tcp"
+                FNAME="${FSYS}/${HST}_tcpdump_${INTF}_port_${PRT}_`date +%d%m%y_%H%M`.pcap"
+                echo "file name to be created $FNAME"
+                echo "Starting tcpdump on  port $PRT  and interface $INTF. File system $FSYS is $RESFS%"
+                2>/dev/null 1>&2 tcpdump -i $INTF -s0 -vv -C 100 -w "$FNAME" &   
+                #TCPDUMP_PID=$!
+                TCPDUMP_PID=`ps -ef |grep tcpdump|grep -v grep |awk '{print $2}'`
+                echo "pid=$TCPDUMP_PID"
+                if [ ${TCPDUMP_PID} ] 
+                then {
+
+                        echo "file created : $FNAME with PID=${TCPDUMP_PID}"
+                        echo "${TCPDUMP_PID}">${TCPDUMP_PID_FILE}
+                } else {
+                        echo "something went wrong pid does not exist"
+                }
+                fi
+
+        }
+        fi
+}
+
+
+
+
+
+#main
+
+check_intf_par
+check_port_par
+check_fs
+start_tcp
---
+++ stop_tcp.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+#stop tcpdump
+# by massimo iannelli
+# last modified 25122016
+# version 1.0
+
+FPID=/var/tmp/tcpdump.pid
+
+
+
+stop_tcpdump () {
+        PIDPAR=$1
+
+        echo "Stopping tcpdump.pid=$TCPDUMP_PID"
+        kill "${PIDPAR}"
+        sleep 1
+        PSCHK=`ps -ef |grep tcpdump|grep -v grep |awk '{print $2}'`
+        if [ ${PSCHK} ]
+        then {
+                kill -9 "${PID_PAR}"
+        } else {
+                echo "tcpdump stopped!"
+                rm $FPID
+        }
+    fi
+}
+
+check_tcpdump () {
+
+	echo "in check tcpdump"
+        if [ -e ${FPID} ]
+        then {
+		echo "in if file with pid found"
+                TCPDUMP_PID=`cat ${FPID}`
+		echo "pid from file=$TCPDUMP_PID"
+                if [ -e /proc/${TCPDUMP_PID} ] && [ ${TCPDUMP_PID} ]
+                then {
+                        echo "found proc pid tcpdump running with pid=${TCPDUMP_PID} stopping it"
+                        kill -9 "${TCPDUMP_PID}"
+                        sleep 1
+                        echo "checking for more processes in memory"
+                        PSCHK=`ps -ef |grep tcpdump|grep -v grep |awk '{print $2}'`
+			if  [ ${PSCHK} ]
+			then {
+			  echo "found pid with ps=$PSCHK"
+			  stop_tcpdump "${PSCHK}" 
+			} else {
+			  echo "no more processes in memory, removing pid file"
+			  rm "${FPID}"
+			} fi
+                } else {
+		  echo "in file pid else no proc processes found"
+		  PSCHK=`ps -ef |grep tcpdump|grep -v grep |awk '{print $2}'`
+		  if  [ ${PSCHK} ]
+		  then {
+		    echo "found pid with ps=$PSCHK"
+		    stop_tcpdump "${PSCHK}" 
+		  } else {
+		    echo "no more processes in memory, removing pid file"
+		    rm "${FPID}"
+		  } fi
+                } fi
+
+        } else {
+		echo "in else no pid "
+                echo "file with pid not found searching running processes"
+                PSCHK1=`ps -ef |grep tcpdump|grep -v grep |awk '{print $2}'|head -1`
+                if [ $PSCHK1 ]
+                then {
+                        echo "res ps =$PSCHK1"
+                        kill $PSCHK1
+                } else {
+		  echo "nothing to do"
+                }
+                fi
+        }
+        fi
+}
+
+check_tcpdump
